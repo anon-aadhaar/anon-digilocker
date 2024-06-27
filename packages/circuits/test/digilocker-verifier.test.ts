92ca9c5719c37676d28918bd9bdf62905871a9e5
Saleel <saleel@saleel.xyz>
2024-06-28 00:26:59 +0400

    add input generator for tests

--- packages/circuits/test/digilocker-verifier.test.ts
+++ packages/circuits/test/digilocker-verifier.test.ts
@@ -1,120 +1,124 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const circom_tester = require('circom_tester/wasm/tester')
+const circom_tester = require("circom_tester/wasm/tester");
 
-import path from 'path'
-import { sha256Pad } from '@zk-email/helpers/dist/sha-utils'
+import path from "path";
+import assert from "assert";
 import {
+  sha256Pad,
+  generatePartialSHA,
+} from "@zk-email/helpers/dist/sha-utils";
+import fs from "fs";
+import crypto from "crypto";
+import XmlDSigJs from "xmldsigjs";
+import {
+  Uint8ArrayToCharArray,
   bigIntToChunkedBytes,
   bufferToHex,
-  Uint8ArrayToCharArray,
-} from '@zk-email/helpers/dist/binary-format'
-import {
-  convertBigIntToByteArray,
-  decompressByteArray,
-  splitToWords,
-  extractPhoto,
-  timestampToUTCUnix,
-} from '@anon-aadhaar/core'
-import fs from 'fs'
-import crypto from 'crypto'
-import assert from 'assert'
-import { buildPoseidon } from 'circomlibjs'
-import { bytesToIntChunks, padArrayWithZeros, bigIntsToString } from './util'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config()
+} from "@zk-email/helpers/dist/binary-format";
 
-let useTestData = true
-let QRData: string = testQRData
-if (process.env.REAL_DATA === 'true') {
-  useTestData = false
-  if (typeof process.env.DIGILOCKER_DATA === 'string') {
-    QRData = process.env.DIGILOCKER_DATA
-  } else {
-    throw Error('You must set .env var DIGILOCKER_DATA when using real data.')
+require("dotenv").config();
+
+XmlDSigJs.Application.setEngine("OpenSSL", globalThis.crypto);
+
+const xml = fs.readFileSync(
+  path.join(__dirname, "./test-data", "pan.xml"),
+  "utf8",
+);
+
+async function prepareTestData() {
+  let doc = XmlDSigJs.Parse(xml);
+  let signature = doc.getElementsByTagNameNS(
+    "http://www.w3.org/2000/09/xmldsig#",
+    "Signature",
+  );
+  let signedXml = new XmlDSigJs.SignedXml(doc);
+  signedXml.LoadXml(signature[0]);
+
+  // @ts-ignore
+  const publicKey = (await signedXml.GetPublicKeys())[0];
+  const publicKeyJWK = await crypto.subtle.exportKey("jwk", publicKey);
+  const pubKeyBigInt = BigInt(
+    "0x" + Buffer.from(publicKeyJWK.n as string, "base64").toString("hex"),
+  );
+
+  const references = signedXml.XmlSignature.SignedInfo.References.GetIterator();
+  if (references.length !== 1) {
+    throw new Error("XML should have exactly one reference");
   }
+
+  // @ts-ignore
+  const signedData = signedXml.ApplyTransforms(
+    references[0].Transforms,
+    doc.documentElement,
+  );
+
+  const signatureB64 = signature[0].getElementsByTagName("SignatureValue")[0]
+    .textContent as string;
+  const signatureBuff = Buffer.from(signatureB64, "base64");
+  const signatureBigInt = BigInt('0x' + signatureBuff.toString("hex"));
+
+  // @ts-ignore
+  const signedInfo = Buffer.from(signedXml.TransformSignedInfo(signedXml));
+
+  const signedDataHaser = crypto.createHash("sha256");
+  signedDataHaser.update(signedData);
+  const signedDataHash = signedDataHaser.digest("base64");
+
+  const dataHashIndex = signedInfo.indexOf(signedDataHash);
+  if (dataHashIndex === -1) {
+    throw new Error("Body hash not found SignedInfo");
+  }
+
+  // Local verification (optional)
+  const rsaResult = crypto.verify(
+    "RSA-SHA1",
+    signedInfo,
+    publicKey,
+    signatureBuff,
+  );
+  assert(rsaResult, "Local: RSA verification failed");
+
+  // inputs
+  const [signedDataPadded, signedDataPaddedLength] = sha256Pad(signedData, 512 * 4)
+  const inputs =  {
+    dataPadded: Uint8ArrayToCharArray(signedDataPadded),
+    dataPaddedLength: signedDataPaddedLength,
+    signedInfo: Uint8ArrayToCharArray(Uint8Array.from(signedInfo)),
+    dataHashIndex: dataHashIndex,
+    signature: bigIntToChunkedBytes(signatureBigInt, 121, 17),
+    pubKey: bigIntToChunkedBytes(pubKeyBigInt, 121,17),
+  }
+
+  return { inputs };
 }
 
+describe("DigiLockerVerifier", function () {
+  this.timeout(0);
 
-function prepareTestData() {
-  const qrDataBytes = convertBigIntToByteArray(BigInt(QRData))
-  const decodedData = decompressByteArray(qrDataBytes)
+  let circuit: any;
 
-  const signatureBytes = decodedData.slice(
-    decodedData.length - 256,
-    decodedData.length,
-  )
+  // this.beforeAll(async () => {
+  //   const pathToCircuit = path.join(
+  //     __dirname,
+  //     '../src',
+  //     'digilocker-verifier.circom',
+  //   )
+  //   circuit = await circom_tester(pathToCircuit, {
+  //     recompile: true,
+  //     // output: path.join(__dirname, '../build'),
+  //     include: [
+  //       path.join(__dirname, '../node_modules'),
+  //       path.join(__dirname, '../../../node_modules'),
+  //     ],
+  //   })
+  // })
 
-  const signedData = decodedData.slice(0, decodedData.length - 256)
+  it("should generate witness for circuit with Sha256RSA signature", async () => {
+    const { inputs } = await prepareTestData();
 
-  const [qrDataPadded, qrDataPaddedLen] = sha256Pad(signedData, 512 * 3)
+    console.log(inputs)
 
-  const delimiterIndices: number[] = []
-  for (let i = 0; i < qrDataPadded.length; i++) {
-    if (qrDataPadded[i] === 255) {
-      delimiterIndices.push(i)
-    }
-    if (delimiterIndices.length === 18) {
-      break
-    }
-  }
-
-  const signature = BigInt(
-    '0x' + bufferToHex(Buffer.from(signatureBytes)).toString(),
-  )
-
-  const pkPem = fs.readFileSync(
-    path.join(__dirname, '../assets', getCertificate(useTestData)),
-  )
-  const pk = crypto.createPublicKey(pkPem)
-
-  const pubKey = BigInt(
-    '0x' +
-      bufferToHex(
-        Buffer.from(pk.export({ format: 'jwk' }).n as string, 'base64url'),
-      ),
-  )
-
-  const inputs = {
-    xmlDataPadded: Uint8ArrayToCharArray(xmlDataPadded),
-    xmlDataPaddedLength: xmlDataPaddedLen,
-    signature: splitToWords(signature, BigInt(121), BigInt(17)),
-    pubKey: splitToWords(pubKey, BigInt(121), BigInt(17)),
-  }
-
-  return {
-    inputs,
-    xmlDataPadded,
-    signedData,
-    pubKey,
-    xmlDataPaddedLen,
-  }
-}
-
-describe('DigiLockerVerifier', function () {
-  this.timeout(0)
-
-  let circuit: any
-
-  this.beforeAll(async () => {
-    const pathToCircuit = path.join(
-      __dirname,
-      '../src',
-      'digilocker-verifier.circom',
-    )
-    circuit = await circom_tester(pathToCircuit, {
-      recompile: true,
-      // output: path.join(__dirname, '../build'),
-      include: [
-        path.join(__dirname, '../node_modules'),
-        path.join(__dirname, '../../../node_modules'),
-      ],
-    })
-  })
-
-  it('should generate witness for circuit with Sha256RSA signature', async () => {
-    const { inputs } = prepareTestData()
-
-    await circuit.calculateWitness(inputs)
-  })
-})
+    // await circuit.calculateWitness(inputs)
+  });
+});
